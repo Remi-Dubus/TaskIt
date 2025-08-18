55df427d04738955094d74a17fac5195ac6acbaa
REMI DUBUS <remdubus@gmail.com>
2025-08-18 15:09:49 +0200

    feat: add onboarding section and welcome page

---
+++ styles/onboardingStyle.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+import { COLORS, FONT_FAMILY, FONT_SIZE } from "@/styles/themes";
+
+export const onboardingStyle = StyleSheet.create({
+    view: {
+        backgroundColor: COLORS.light,
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 30
+    },
+    picture: {
+        width: 160,
+        height: 160,
+    },
+    title:{
+        fontFamily: FONT_FAMILY.title
+    },    
+    text: {
+        marginBottom: 30,
+        fontFamily: FONT_FAMILY.paragraphe,
+        fontSize: FONT_SIZE.secondTitle,
+        color: COLORS.dark,
+        textAlign: "center",
+    }
+});
